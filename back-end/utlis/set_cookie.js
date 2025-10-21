510149a832073cce88d82fa9b3a13ac4b7fcce44
hussainshahid12 <161245532+hussainshahid12@users.noreply.github.com>
2025-10-21 18:09:07 +0500

    Enhance cookie security settings in setCookie functionhttp
    Updated cookie settings to ensure secure handling in production.

--- back-end/utlis/set_cookie.js
+++ back-end/utlis/set_cookie.js
@@ -1,8 +1,11 @@
 function setCookie(res, token) {
   const expires = new Date(Date.now() + 10 * 1000);
 
+
+  // http true means only acces server not a client side  and secure false if not a production mode set in a best practice otherwise proper work in a true mode
   res.cookie("token", token, {
-    httpOnly: true,
+    
+    httpOnly: true,  
     secure: true,
     maxAge: 3600 * 1000, // 1 hour in milliseconds
   });
@@ -13,5 +16,16 @@ function setCookie(res, token) {
     maxAge: 3600 * 1000, // 1 hour in milliseconds
   });
 }
+
+
+
+
+
+// res.cookie("token", token, {
+//   httpOnly: false,
+//   expires: expires,
+//   secure: false, // Should be true in production with HTTPS
+// });
+
 
 module.exports = setCookie;
